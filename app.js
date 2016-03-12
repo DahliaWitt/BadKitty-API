f0fd0b8b6e0943a190ca6fbdee167135814cc7ca
Drake Witt <dwitt@dranweb.com>
2016-03-12 12:55:23 -0500

    Fixed cors (I think)

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('express');
 var path = require('path');
 var config = require('./config/environment');
+var cors = require('cors')
+
 
 var app = express();
 
@@ -14,11 +16,7 @@ require('./routes.js')(app);
 app.listen(process.env.PORT || 80);
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 app.on('connection', function(socket) {
   console.log('connection made');
