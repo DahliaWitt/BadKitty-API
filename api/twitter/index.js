303d12e9b52145c94ba66ff58065fe86b22559ff
Drake Witt <dwitt@dranweb.com>
2016-03-12 13:30:27 -0500

    Should totally fix CORS issue

--- api/twitter/index.js
+++ api/twitter/index.js
@@ -6,5 +6,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/*?', controller.get);
+router.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 module.exports = router;
