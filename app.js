625bbd8f90bfb2a98a8c4995b9101e2fa9d9bde7
Drake Witt <dwitt@dranweb.com>
2016-04-01 07:28:22 -0400

    Started working on mirror

--- app.js
+++ app.js
@@ -6,8 +6,17 @@ var express = require('express');
 var path = require('path');
 var config = require('./config/environment');
 
+var socket = require('socket.io-client')('https://www.mctl.gq:3000');
 
 var app = express();
+var server = require('http').Server(app);
+ 
+var io = require('socket.io')(server);
+
+
+
+
+
 
 require('./config/express')(app);
 require('./routes.js')(app);
@@ -21,5 +30,20 @@ app.on('connection', function(socket) {
   socket.setTimeout(100);
 });
 console.log('Listening on ' + config.port + ' in ' + app.get('env') + ' mode...');
+
+  socket.on('connect', function(){});
+  socket.on('event', function(data){});
+  socket.on('disconnect', function(){});
+socket.emit('last-track');
+socket.on('new-track', function(data) {
+    console.log(data);
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
 
 exports = module.exports = app;
