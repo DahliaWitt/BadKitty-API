b63ca23fb81e52fe7e760df9605551617994ad4b
Drake Witt <dwitt@dranweb.com>
2016-05-31 12:45:31 -0400

    Added instagram proxy

--- routes.js
+++ routes.js
@@ -5,7 +5,8 @@ module.exports = function(app)
   app.use('/api/audio-stream', require('./api/audio-stream'));
   app.use('/api/stream-chunks', require('./api/stream-chunks'));
   app.use('/api/chunk-proxy', require('./api/chunk-proxy'));
-  app.use('/api/twitter', require('./api/twitter'))
+  app.use('/api/twitter', require('./api/twitter'));
+  app.use('/api/proxy', require('./api/proxy'));
   app.get('/', function(req, res){
     res.render('index');
   })
